86b57941896f1bd5cf6dde34adf0cdb54618649c
RimshaNazar <rimshanazar43@gmail.com>
2024-10-19 16:48:51 +0100

    Add files via upload

---
+++ G.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int stud[5][2]={
+    {1234,56},
+    {1212,33},
+    {1434,80},
+    {1312,78},
+    {1203,75}
+};
+    int i,j;
+    for(i=0; i<=4; i++)
+ {printf("address of %d th 1-D array=%u",i,stud[i]);
+}}
+
+
+
