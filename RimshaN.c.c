6edf12d6bb8d83577724f81554362d2eaa3985c3
RimshaNazar <rimshanazar43@gmail.com>
2024-10-19 17:59:29 +0100

    Add files via upload

---
+++ RimshaN.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char destinaton[25];
+    char *blank=" ",*c="c++",*dev="dev";
+    strcpy(destination,dev); 
+	strcat(destination,blank);
+    strcat(destination,c);
+    return 0;
+}
