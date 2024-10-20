e010ae0ee69926f06097876165506663fb0f4783
RimshaNazar <rimshanazar43@gmail.com>
2024-10-20 04:49:01 +0100

    Add files via upload

--- RimshaN.c.c
+++ RimshaN.c.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 int main(){ 
-   char string[10];
-   char *str1 = "abcdefghi";
-   strcpy(string,str1);
-   printf("%s",string);
+   char *buf1="aaa",*buf2="bbb",*buf3="ccc";
+   int ptr;
+   ptr=strcmp(buf1,buf2);
+   if(ptr>0)
+   printf("buffer 2 is greater than buffer 1");
+   else
+   printf("buffer 2 is less than buffer 1");
+   ptr=strcmp(buf2,buf3);
+   if (ptr>0)
+   printf("buffer 2 is greater than buffer 3");
+   else
+   printf("buffer 2 is less than buffer 3");
    return 0;
 }
    
